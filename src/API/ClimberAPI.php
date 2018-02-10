2ccee771ffe06482a5a328eaf3e5c785cf633cb2
Ben Martinez-Bateman <ben@alwaysblank.org>
2018-02-10 15:25:58 -0800

    Adds Climber API.

---
+++ src/API/ClimberAPI.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Livy\Climber\API;
+
+use Livy\Climber\Tree;
+
+/**
+ * This defines the API for the Livy\Climber to help insure consistent behavior
+ * in future versions of the software.
+ * @package Livy\Climber\API
+ */
+abstract class ClimberAPI
+{
+    /**
+     * Sets up the `$tree`, and adds active classes to the appropriate leaves,
+     * if `$currentUrl` matches to a leaf target.
+     *
+     * @param Tree $tree
+     * @param string $currentUrl
+     */
+    abstract public function __construct(Tree $tree, $currentUrl = null);
+
+    /**
+     * If `Climber` is treated as a string, print out a <ul></ul>.
+     *
+     * @return string
+     */
+    abstract public function __toString();
+
+    /**
+     * Get the value of a property.
+     *
+     * Don't try to get non-existent properties. If you want to modify what someone
+     * gets, do it here.
+     *
+     * @param string $property  Name of property to get.
+     * @return mixed            Returns bool `false` if property does not exist.
+     */
+    abstract public function __get(string $property);
+
+    /**
+     * Set a property, if that is possible.
+     *
+     * Requested property is only set if its name appears
+     * in `Climber::setable`.
+     *
+     * If this object has a method named `set[Property]`
+     * (i.e. for the property `tree` it would be `setTree`)
+     * then `$value` is passed to it before being
+     * added.
+     *
+     * @param string $property      Property we want to set.
+     * @param mixed $value          Value we want to set $property to.
+     * @return mixed                $value if $this->$property is setable, bool `false` otherwise.
+     */
+    abstract public function __set(string $property, $value = null);
+
+    /**
+     * Activate a leaf.
+     *
+     * This sets some stuff on leaves to make them 'active' in various ways.
+     * The primary purpose of this is to highlight menu items when the user is
+     * on that page.
+     *
+     * @param integer $hint
+     * @return void
+     */
+    abstract public function activate(int $hint);
+
+    /**
+     * Gets zero or more leaves, based on their target.
+     *
+     * This is primarily useful when you want to set active leaves for the
+     * current page. It returns an array containing all leaves with this
+     * target. This means the array can contain no leaves!
+     *
+     * If `$strict` is `true`, then it just does a direct string match test. If
+     * `$strict` is `false`, then it tests the path, queries, and fragments
+     * against one another with `parse_url`.
+     *
+     * @param string $target
+     * @param boolean $strict
+     * @return array
+     */
+    abstract public function getLeafByTarget(string $target, bool $strict = true);
+
+    /**
+     * Set $this->topAttr.
+     *
+     * @see \Livy\Climber\Climber::appendArrayProp()
+     *
+     * @param $value
+     * @param $property
+     * @return array
+     */
+    abstract public function setTopAttr($value, $property);
+
+    /**
+     * Set $this->menuAttr.
+     *
+     * @see \Livy\Climber\Climber::appendArrayProp()
+     *
+     * @param $value
+     * @param $property
+     * @return array
+     */
+    abstract public function setMenuAttr($value, $property);
+
+    /**
+     * Set $this->itemAttr.
+     *
+     * @see \Livy\Climber\Climber::appendArrayProp()
+     *
+     * @param $value
+     * @param $property
+     * @return array
+     */
+    abstract public function setItemAttr($value, $property);
+
+    /**
+     * Set $this->linkAttr.
+     *
+     * @see \Livy\Climber\Climber::appendArrayProp()
+     *
+     * @param $value
+     * @param $property
+     * @return array
+     */
+    abstract public function setLinkAttr($value, $property);
+
+    /**
+     * Hook up a callback to a processing step.
+     *
+     * Ultimately callbacks are run through `call_user_func()`, so you
+     * can pass anything to $callback that would be accepted as an
+     * argument for `call_user_func()`.
+     *
+     * @param string $location
+     * @param mixed $callback
+     * @param int|boolean $order
+     * @return void
+     */
+    abstract public function hook(string $location, $callback, $order = false);
+
+    /**
+     * Return (or optionally echo) the full HTML for the menu.
+     *
+     * @param boolean $echo
+     * @return string
+     */
+    abstract public function element($echo = false);
+}
--- src/Climber.php
+++ src/Climber.php
@@ -4,7 +4,7 @@ namespace Livy\Climber;
 
 use \Zenodorus as Z;
 
-class Climber
+class Climber extends API\ClimberAPI
 {
     protected $setable = [
       'tree',
@@ -67,12 +67,6 @@ class Climber
     protected $itemOutputHooks = [];
     protected $linkHooks = [];
 
-    /**
-     * Sets up the `$tree`, and adds active classes to the appropriate leaves,
-     * if `$currentUrl` matches to a leaf target.
-     *
-     * @param Tree $tree
-     */
     public function __construct(Tree $tree, $currentUrl = null)
     {
         $this->tree = $tree;
@@ -137,22 +131,11 @@ class Climber
         }
     }
 
-  /**
-   * If `Climber` is treated as a string, print out a <ul></ul>.
-   *
-   * @return string
-   */
     public function __toString()
     {
         return $this->element();
     }
 
-  /**
-   * Get the value of a property.
-   *
-   * @param string $property  Name of property to get.
-   * @return mixed            Returns bool `false` if property does not exist.
-   */
     public function __get(string $property)
     {
         if (property_exists($this, $property)) {
@@ -161,22 +144,7 @@ class Climber
 
         return null;
     }
-
-  /**
-   * Set a property, if that is possible.
-   *
-   * Requested property is only set if its name appears
-   * in `Climber::setable`.
-   *
-   * If this object has a method named `set[Property]`
-   * (i.e. for the property `tree` it would be `setTree`)
-   * then `$value` is passed to it before being
-   * added.
-   *
-   * @param string $property      Property we want to set.
-   * @param mixed $value          Value we want to set $property to.
-   * @return mixed                $value if $this->$property is setable, bool `false` otherwise.
-   */
+    
     public function __set(string $property, $value = null)
     {
         if (in_array($property, $this->setable)) {
@@ -193,16 +161,6 @@ class Climber
         return null;
     }
 
-    /**
-     * Activate a leaf.
-     *
-     * This sets some stuff on leaves to make them 'active' in various ways.
-     * The primary purpose of this is to highlight menu items when the user is
-     * on that page.
-     *
-     * @param integer $hint
-     * @return void
-     */
     public function activate(int $hint)
     {
         if ($leaf = $this->tree->getLeaf($hint)) {
@@ -228,21 +186,6 @@ class Climber
         }
     }
 
-    /**
-     * Gets zero or more leaves, based on their target.
-     *
-     * This is primarily useful when you want to set active leaves for the
-     * current page. It returns an array containing all leaves with this
-     * target. This means the array can contain no leaves!
-     *
-     * If `$strict` is `true`, then it just does a direct string match test. If
-     * `$strict` is `false`, then it tests the path, queries, and fragments
-     * against one another with `parse_url`.
-     *
-     * @param string $target
-     * @param boolean $strict
-     * @return array
-     */
     public function getLeafByTarget(string $target, bool $strict = true)
     {
         $leaves = [];
@@ -290,41 +233,21 @@ class Climber
         return $temp;
     }
 
-    /**
-     * Set $this->topAttr.
-     *
-     * @see Livy\Climber\Climber::appendArrayProp()
-     */
     public function setTopAttr($value, $property)
     {
         return $this->appendArrayProp($value, $property);
     }
 
-    /**
-     * Set $this->menuAttr.
-     *
-     * @see Livy\Climber\Climber::appendArrayProp()
-     */
     public function setMenuAttr($value, $property)
     {
         return $this->appendArrayProp($value, $property);
     }
 
-    /**
-     * Set $this->itemAttr.
-     *
-     * @see Livy\Climber\Climber::appendArrayProp()
-     */
     public function setItemAttr($value, $property)
     {
         return $this->appendArrayProp($value, $property);
     }
 
-    /**
-     * Set $this->linkAttr.
-     *
-     * @see Livy\Climber\Climber::appendArrayProp()
-     */
     public function setLinkAttr($value, $property)
     {
         return $this->appendArrayProp($value, $property);
@@ -424,18 +347,6 @@ class Climber
         return null;
     }
 
-    /**
-     * Hook up a callback to a processing step.
-     *
-     * Ultimately callbacks are run through `call_user_func()`, so you
-     * can pass anything to $callback that would be accepted as an
-     * argument for `call_user_func()`.
-     *
-     * @param string $location
-     * @param mixed $callback
-     * @param int|boolean $order
-     * @return void
-     */
     public function hook(string $location, $callback, $order = false)
     {
         $propName = sprintf("%sHooks", $location);
@@ -512,7 +423,7 @@ class Climber
    * Sprout a link.
    *
    * @param array $bud
-   * @return void
+   * @return string
    */
     protected function fruit(array $bud)
     {
@@ -587,7 +498,7 @@ class Climber
      * all top-level items have no parents).
      *
      * @param Tree $tree
-     * @return string
+     * @return array
      */
     protected function harvest(Tree $tree)
     {
@@ -600,14 +511,6 @@ class Climber
         ];
     }
 
-
-
-    /**
-     * Return (or optionally echo) the full HTML for the menu.
-     *
-     * @param boolean $echo
-     * @return string
-     */
     public function element($echo = false)
     {
         if (null != $this->tree->grow()) {
